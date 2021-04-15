678234866b294c9a2497e02831c178df1672af32
kimyejin <7602kyj@e-mirim.hs.kr>
2021-04-16 00:14:08 +0900

    gradle 추가 및 주석

--- app/src/main/java/com/example/yjbase_object_android/MainActivity.java
+++ app/src/main/java/com/example/yjbase_object_android/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
